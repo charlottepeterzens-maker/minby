db1c9c1b1511d586602ff9fc05d9df19c5587fd5
charlottepeterzens-maker <charlottepeterzens@gmail.com>
2026-03-22 21:06:25 +0100

    Update ContentFeed.tsx

--- src/components/feed/ContentFeed.tsx
+++ src/components/feed/ContentFeed.tsx
@@ -1,1 +1,15 @@
+import { FeedItemRenderer } from "./FeedItemRenderer";
 
+type Props = {
+  items: any[];
+};
+
+export const ContentFeed = ({ items }: Props) => {
+  return (
+    <>
+      {items.map((item) => (
+        <FeedItemRenderer key={item.data.id} item={item} />
+      ))}
+    </>
+  );
+};
